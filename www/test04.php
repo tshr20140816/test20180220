5880d8b41c1c985b3cbfca782971b5c896925a8e
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-14 14:37:31 +0900

    Update test04.php

--- www/test04.php
+++ www/test04.php
@@ -3,7 +3,7 @@ $pid = pcntl_fork();
 if ($pid == -1) {
      error_log('***** not fork *****');
 } else if ($pid) {
-    error_log("***** parent process *****";
+    error_log("***** parent process *****");
     pcntl_wait($status);
 } else {
     // 子プロセスの場合
