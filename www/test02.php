93eb3291134b27f4ac88d92fe5689c5e93fb8cd2
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-14 11:06:10 +0900

    Update test02.php

--- www/test02.php
+++ www/test02.php
@@ -11,6 +11,37 @@ for($i = 0; $i < 10; $i++)
 
 $mh = curl_multi_init();
 
+foreach($urls as $url)
+{
+  error_log($url);
+  
+  $conn = curl_init($url);
+  curl_multi_add_handle($mh, $conn);
+}
 
+$active = null;
+  
+do
+{
+  $mrc = curl_multi_exec($mh, $active);
+} while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+while ($active and $mrc == CURLM_OK)
+{
+  if (curl_multi_select($mh) != -1)
+  {
+    do
+    {
+      $mrc = curl_multi_exec($mh, $active);
+    } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+  }
+}
+
+if ($mrc != CURLM_OK)
+{
+  error_log(' ***** ERROR *****');
+}
+
+curl_multi_close($mh);
 
 ?>
