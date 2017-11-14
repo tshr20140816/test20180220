7ec31153d4f98686691492062b42c13ae04f13ee
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-14 11:45:42 +0900

    Update test03.php

--- www/test03.php
+++ www/test03.php
@@ -1,10 +1,18 @@
 <?php
 $pid = getmypid();
-$param = $_GET['p'];
+$count = $_GET['c'];
+$url = $_GET['u'];
 
 error_log("${pid} START ${param}");
 
 sleep(5);
+
+$count++;
+
+if ($count < 10)
+{
+  file_get_contents($url . '?c=' . $count + '&u=' . $url);
+}
 
 error_log("${pid} FINISH ${param}");
 
