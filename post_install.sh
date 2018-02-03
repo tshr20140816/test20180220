1b97e309184977076bca7448dad30a53be9708b6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-03 18:10:34 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -5,11 +5,11 @@ set -x
 date
 
 pushd www
-gzip -9c test.css > test.css.gz
+gzip -c test.css > test.css.gz
 rm -f test.css
 
 wget https://git.tt-rss.org/fox/tt-rss/raw/17.4/css/tt-rss.css
-gzip -9ac tt-rss.css > tt-rss.css.gz
+gzip -c tt-rss.css > tt-rss.css.gz
 rm -f tt-rss.css
 
 popd
