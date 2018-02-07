d51b9ade1317c91322967f2ee91998b1dacd90ff
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-07 11:01:22 +0900

    Update test01.php

--- www/test01.php
+++ www/test01.php
@@ -2,8 +2,10 @@
 
 $res = file_get_contents('http://ipaddress.is/8.8.8.8');
 
-$pos = strpos($res, '<b>IP Whois Information');
+$pos_start = strpos($res, '<b>IP Whois Information');
 
-error_log(substr($res, $pos));
+$pos_end = strpos(substr($res, $pos_start), '</tbody>');
+
+error_log(substr($res, $pos_start, $pos_end - $pos_start));
 
 ?>
