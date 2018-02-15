8b8f49222ae0d185444c75fdbcf09ed9470bfee0
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-15 20:17:16 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -11,8 +11,14 @@ wget https://github.com/tshr20140816/test20171031/raw/master/usr.zip
 
 unzip usr.zip
 
-cd usr
+cd ${HOME2}
 
-ls -lang
+wget http://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.29.tar.gz
+tar xf httpd-2.4.29.tar.gz
+rm httpd-2.4.29.tar.gz
+cd httpd-2.4.29
+./configure --help
+./configure --prefix=/tmp/usr --with-apr=/tmp/usr --enable-ssl --enable-http2 --with-nghttp2=/tmp/usr
+make -j$(grep -c -e processor /proc/cpuinfo)
 
 date
