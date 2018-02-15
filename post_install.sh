0023f92493658248bc8ecd319f2330bdc0a5e7ad
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-15 17:37:48 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,4 +6,16 @@ date
 
 openssl version
 
+wget https://github.com/nghttp2/nghttp2/releases/download/v1.30.0/nghttp2-1.30.0.tar.xz
+tar xf nghttp2-1.30.0.tar.xz
+cd nghttp2-1.30.0
+./configure --help
+./configure --prefix=/tmp/usr/
+make -j
+make install
+
+ls -Rlang /tmp/usr/
+
+cd ~
+
 date
