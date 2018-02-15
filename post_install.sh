14c8732e44b87c23a4b415e1cb7a4eade441c205
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-15 15:27:04 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -56,7 +56,10 @@ ls -Rlang /tmp/usr/
 ls -lang
 
 ./configure  --help
-./configure  --enable-proxy-http2 --enable-http2 --enable-proxy-http --with-nghttp2=/tmp/usr
+./configure  --enable-proxy-http2 --enable-http2 --enable-proxy-http --with-nghttp2=/tmp/usr --prefix=/tmp/usr2
 
-# make -j$(grep -c -e processor /proc/cpuinfo)
+make -j$(grep -c -e processor /proc/cpuinfo)
+
+make install
+
 date
