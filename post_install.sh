d8081b1791f244e3948467fd798e4466b03844d1
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-15 20:20:27 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -19,6 +19,7 @@ rm httpd-2.4.29.tar.gz
 cd httpd-2.4.29
 ./configure --help
 ./configure --prefix=/tmp/usr --with-apr=/tmp/usr --enable-ssl --enable-http2 --with-nghttp2=/tmp/usr
-make -j$(grep -c -e processor /proc/cpuinfo)
+# make -j$(grep -c -e processor /proc/cpuinfo)
+make
 
 date
