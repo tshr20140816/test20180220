7af2f24fa6ad517d1cc706b60861d9b0eb88b38c
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-21 00:36:37 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -4,11 +4,4 @@ set -x
 
 export TZ=JST-9
 
-ls -lang
-mkdir -p /tmp/usr/lib
-cp libnghttp2.so.14 /tmp/usr/lib/
-
-ldd ./mod_http2.so
-ldd ./mod_proxy_http2.so
-
 vendor/bin/heroku-php-apache2 -C apache.conf www
