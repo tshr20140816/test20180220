939430c5bc5bb3578572e2ccf31304ef77fcba1d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-22 00:13:04 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -15,16 +15,18 @@ wget https://git.tt-rss.org/fox/tt-rss/raw/17.4/css/tt-rss.css
 gzip -c tt-rss.css > tt-rss.css.gz
 rm tt-rss.css
 
-cd /tmp
+export PATH="/tmp/usr/bin:${PATH}"
+mkdir -m 777 -p /tmp/usr
 
+cd /tmp/usr
 wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.tar.gz
-tar xf cmake-3.10.2-Linux-x86_64.tar.gz
-cd cmake*
+tar xf --strip=1 cmake-3.10.2-Linux-x86_64.tar.gz
 ls -lang
 
-# git clone --depth 1 https://github.com/google/brotli
-# cd brotli
-# ./bootstrap
+cd /tmp
+git clone --depth 1 https://github.com/google/brotli
+cd brotli
+./configure-cmake
 # make -j2
 
 date
