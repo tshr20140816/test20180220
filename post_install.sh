08cc4f1e8ca5be84fc45a998abaefe39efea63c6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-04-02 11:51:15 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,42 +6,19 @@ date
 
 chmod 777 start_web.sh
 
-cd www
+pushd /tmp
 
-gzip -c test.css > test.css.gz
-rm test.css
+wget https://osdn.jp/projects/nkf/downloads/64158/nkf-2.1.4.tar.gz
+tar xf nkf-2.1.4.tar.gz
+cd nkf-2.1.4
+make
 
-wget https://git.tt-rss.org/fox/tt-rss/raw/17.4/css/tt-rss.css
-gzip -c tt-rss.css > tt-rss.css.gz
-rm tt-rss.css
-
-
-
-export PATH="/tmp/usr/bin:${PATH}"
-mkdir -m 777 -p /tmp/usr
-
-cd /tmp/usr
-wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.tar.gz
-tar xf cmake-3.10.2-Linux-x86_64.tar.gz -C ./ --strip=1 
 ls -lang
 
-cd /tmp
-git clone --depth 1 https://github.com/google/brotli
-cd brotli
-./configure-cmake --help
-./configure-cmake --prefix=/tmp/usr
-make -j2
-make install
+popd
 
-ls -lang /tmp/usr/bin/
+mv /tmp/nkf-2.1.4/nkf ./
 
-brotli --help
-
-cd /tmp
-
-wget https://git.tt-rss.org/fox/tt-rss/raw/17.4/css/tt-rss.css
-gzip -9c tt-rss.css > tt-rss.css.gz
-brotli -9 -o tt-rss.css.br tt-rss.css
 ls -lang
 
 date
